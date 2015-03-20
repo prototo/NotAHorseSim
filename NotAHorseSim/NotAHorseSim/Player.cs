85a00b1c61674a8f50b20e15036344f4a1e796d5
Greg <gmackelden@gmail.com>
2015-03-20 21:25:58 +0000

    Done the game

---
+++ NotAHorseSim/NotAHorseSim/Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NotAHorseSim
+{
+    class Player
+    {
+        public Texture2D texture;
+        public Vector2 position;
+
+        public bool active;
+        public byte health;
+
+        public void Initialize(Texture2D playerTexture, Vector2 playerPosition)
+        {
+            texture = playerTexture;
+            position = playerPosition;
+
+            active = true;
+            health = 100;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position);
+        }
+
+        public int getWidth()
+        {
+            return texture.Width;
+        }
+
+        public int getHeight()
+        {
+            return texture.Height;
+        }
+    }
+}
